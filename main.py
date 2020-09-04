e02d86427f0801dcdff28132fb3521388db09dec
Lucas Hourahine <luca5h1905@gmail.com>
2020-09-04 12:49:37 -0400

    working

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
 import random
+import json
 
-statements = ["Hello"]
-responses = {}
-
-def startbot():
+input1 = input("Load file? (Y/n) ")
+if input1 == "y" or input1 == "Y" or input1 == "":
+	with open('statements.txt', 'r+') as file:
+		statements = json.load(file)
+	with open('responses.txt', 'r+') as file:
+		responses = json.load(file)
+else:
+	with open('statements.txt', 'w+') as file:
+		statements = ["hello"]
+		file.write(json.dumps(statements))
+	with open('responses.txt', 'w+') as file:
+		responses = {}
+		file.write(json.dumps(responses))
+		
+def main():
 	usrinput = 0
 	while True:
 		current_statement = statements[random.randint(0, len(statements)-1)]
 		print("Bot: " + current_statement)
 		while True:	
 			usrinput = input("You: ")
+			usrinput.lower()
 			is_in_s = usrinput in statements
 			if is_in_s == True:
 				if usrinput in responses.keys():
-					print("Bot: " + responses[usrinput])
-					current_statement = responses[usrinput]
+					print("DEBUG: recognized")
+					value = random.choice(responses[usrinput])
+					print("Bot: " + value)
+					current_statement = value
 				else:
 					print("Bot: " + usrinput)
+					print("DEBUG: unrecognized")
 					current_statement = usrinput
+			elif usrinput == "/quit" or usrinput == "/exit":
+				jstatements = json.dumps(statements)
+				jresponses = json.dumps(responses)
+				with open('statements.txt', 'w+') as file:
+					file.write(jstatements)
+				with open('responses.txt', 'w+') as file:
+					file.write(jresponses)
+				quit()
 			else:
 				statements.append(usrinput)
-				responses[current_statement] = usrinput
+				responses.setdefault(current_statement, [])
+				if usrinput in responses[current_statement]:
+					pass
+				else:
+					responses[current_statement].append(usrinput)
 				break
-def main():
-	startbot()
 
 main()
