db77ce3de86b41b2e696ef6664559a11aa9f569b
Lucas Hourahine <luca5h1905@gmail.com>
2020-09-04 10:48:27 -0400

    lol this is funny

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ statements = ["Hello"]
 responses = {}
 
 def startbot():
+	usrinput = 0
 	while True:
 		current_statement = statements[random.randint(0, len(statements)-1)]
 		print("Bot: " + current_statement)
-		usrinput = input("You: ")
-		is_in_s = usrinput in statements
-		if is_in_s == True:
-			responses[current_statement] = usrinput
-		elif is_in_s == False:
-			statements.append(usrinput)
-			responses[current_statement] = usrinput
+		while True:	
+			usrinput = input("You: ")
+			is_in_s = usrinput in statements
+			if is_in_s == True:
+				if usrinput in responses.keys():
+					print("Bot: " + responses[usrinput])
+					current_statement = responses[usrinput]
+				else:
+					print("Bot: " + usrinput)
+					current_statement = usrinput
+			else:
+				statements.append(usrinput)
+				responses[current_statement] = usrinput
+				break
 def main():
 	startbot()
 
