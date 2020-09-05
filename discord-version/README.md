e57deac2f440d4be0b3539c6022f5102cbb2b88c
Lucas Hourahine <luca5h1905@gmail.com>
2020-09-05 14:22:02 -0400

    add discord version

---
+++ discord-version/README.md
@@ -0,0 +1,3 @@
+## Hello-bot Discord Version
+
+This is the Discord implementation. Create a Discord app, then bot, then run this program with the tokens substituted.
---
+++ discord-version/main.py
@@ -0,0 +1,53 @@
+import os
+import random
+import json
+import discord
+
+## BOT VARIABLES
+statements = ["hello"]
+responses = {}
+current_statement = "hello"
+
+TOKEN = '[BOT TOKEN]'
+
+client = discord.Client()
+channel = client.get_channel([CHANNEL TOKEN])
+
+@client.event
+#async def on_join():
+#	await channel.send(current_statement)
+	
+async def on_message(message):
+	global current_statement
+	if message.channel.id == [CHANNEL TOKEN]:
+		channel = message.channel
+		if message.author == client.user:
+			return
+		## BOT CODE
+		usrinput = message.content.lower()
+		is_in_s = usrinput in statements
+		if is_in_s == True:
+			if usrinput in responses.keys():
+				value = random.choice(responses[usrinput])
+				await channel.send(value)
+				current_statement = value
+			else:
+				await channel.send(usrinput)
+				current_statement = usrinput
+		else:
+			statements.append(usrinput)
+			responses.setdefault(current_statement, [])
+			if usrinput in responses[current_statement]:
+				pass
+			else:
+				responses[current_statement].append(usrinput)
+			current_statement = statements[random.randint(0, len(statements)-1)]
+			await channel.send(current_statement)
+		jstatements = json.dumps(statements)
+		jresponses = json.dumps(responses)
+		with open('statements.txt', 'w+') as file:
+			file.write(jstatements)
+		with open('responses.txt', 'w+') as file:
+			file.write(jresponses)
+			
+client.run(TOKEN)
